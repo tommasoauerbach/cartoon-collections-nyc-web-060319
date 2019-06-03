011670b9c85919d137a517ba38476312661da56e
tommaso auerbach <tsa302@nyu.edu>
2019-06-03 01:23:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,16 @@
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do { |dwarves, index| roll_call_dwarves[dwarves] = index }
+  dwarves.each_with_index do |dwarves, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
